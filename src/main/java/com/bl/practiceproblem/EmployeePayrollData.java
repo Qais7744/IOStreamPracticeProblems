2a06acb97452ae8deee02318e2b2b0efebbab5f1
unknown <altamashbagwan1@gmail.com>
2021-11-09 02:01:12 +0530

    Added: Fuctioning of Read and Write data by IOStream

---
+++ src/main/java/com/bl/practiceproblem/EmployeePayrollData.java
@@ -0,0 +1,21 @@
+package com.bl.practiceproblem;
+
+public class EmployeePayrollData {
+    public int id;
+    public String name;
+    public double salary;
+
+    public EmployeePayrollData(Integer id, String name, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return "EmployeePayrollData {" +
+                "id = " + id +
+                ", name = '" + name + '\'' +
+                ", salary = " + salary +
+                '}';
+    }
+}
